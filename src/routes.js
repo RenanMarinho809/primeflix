12e437c494f0a77ffdda573902837b3fdff3705b
RenanMarinho809 <renanmarinho809@gmail.com>
2025-11-10 19:49:05 -0300

    Implement routing with Header component and update Home page title

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Header from "./components/Header";
 
 export default function RoutesApp() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
